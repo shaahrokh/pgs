09e34b65c935db1191f0a862a0bc78c6143e98dc
Neo <neo@pop-os.localdomain>
2021-12-26 23:44:55 +0330

    add posgtres conf and edit pgs docker compose

---
+++ app/backend/src/plugins/db.ts
@@ -0,0 +1,28 @@
+import fp from 'fastify-plugin'
+const { Client } = require('pg')
+
+const client  = new Client({
+    user: "admin",
+    host: "127.0.0.1",
+    password: "a1s2d3f4",
+    database: "pgs",
+});
+
+export interface SupportPluginOptions {
+  // Specify Support plugin options here
+}
+
+// The use of fastify-plugin is required to be able
+// to export the decorators to the outer scope
+export default fp<SupportPluginOptions>(async (fastify, opts) => {
+  fastify.decorate('connection', function () {
+    client.connect();
+  })
+})
+
+// When using .decorate you have to specify added properties for Typescript
+declare module 'fastify' {
+  export interface FastifyInstance {
+    connection(): string;
+  }
+}
--- app/backend/src/routes/roots.ts
+++ app/backend/src/routes/roots.ts
@@ -1,9 +1,18 @@
 import { FastifyPluginAsync } from "fastify";
 
-const root: FastifyPluginAsync =async (fastify, opts): Promise<void> => {
-    fastify.get('/',async (request, reply) => {
-        return {root: true};
-    })    
+const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+
+    fastify.route({
+        method: 'GET',
+        url: '/',
+        handler :async (request, reply) => {
+            return {root: true}
+        }
+    });
+
+    // fastify.get('/',async (request, reply) => {
+    //     return {root: true};
+    // })    
 }
 
 export default root;
